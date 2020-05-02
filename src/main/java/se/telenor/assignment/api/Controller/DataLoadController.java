18f0a7f88baa940ef19fadea6c8d85cb111fbbf7
Sarath <sarathjnair@gmail.com>
2020-05-03 00:52:13 +0200

    [] Loading data from CSV initial commit.

---
+++ src/main/java/se/telenor/assignment/api/Controller/DataLoadController.java
@@ -0,0 +1,42 @@
+package se.telenor.assignment.api.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.telenor.assignment.api.model.Product;
+import se.telenor.assignment.api.model.ProductModel;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/dataload")
+public class DataLoadController {
+
+  @GetMapping()
+  public void loadCSVToDB() throws Exception {
+    BufferedReader subFileBufferedReader =
+        new BufferedReader(
+            new InputStreamReader(DataLoadController.class.getResourceAsStream("/data.csv")));
+    String[] subFileColumnNames = subFileBufferedReader.readLine().split(",");
+    System.out.println("subFileColumnNames: " + subFileColumnNames.length);
+   Arrays.stream(subFileColumnNames).forEach(s -> System.out.println("Value :: "+s));
+
+      String[] key;
+      String line;
+      while (Objects.nonNull((line = subFileBufferedReader.readLine()))) {
+          key = new String[4];
+          String[] lineArray = line.split(",", -1);
+          for(int i=0;i<4;i++){
+              System.out.println("Real :: "+lineArray[i]);
+          }
+      }
+
+  }
+}
