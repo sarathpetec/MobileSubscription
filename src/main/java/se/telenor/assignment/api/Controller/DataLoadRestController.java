9b78beb33d98f28e8759bdfdec7adba8f891785d
Sarath <sarathjnair@gmail.com>
2020-05-03 12:28:12 +0200

    [] restructure the code base.

--- src/main/java/se/telenor/assignment/api/Controller/DataLoadRestController.java
+++ src/main/java/se/telenor/assignment/api/Controller/DataLoadRestController.java
@@ -9,12 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import se.telenor.assignment.api.model.Product;
 import se.telenor.assignment.api.service.DataLoadService;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/dataload")
@@ -25,30 +20,7 @@ public class DataLoadRestController {
 
   @GetMapping()
   public ResponseEntity<List<Product>> loadCSVToDB() throws Exception {
-    List<Product> productList = new ArrayList<>();
-    BufferedReader subFileBufferedReader =
-        new BufferedReader(
-            new InputStreamReader(DataLoadRestController.class.getResourceAsStream("/data.csv")));
-    String[] subFileColumnNames = subFileBufferedReader.readLine().split(",");
-    System.out.println("subFileColumnNames: " + subFileColumnNames.length);
-    Arrays.stream(subFileColumnNames).forEach(s -> System.out.println("Value :: " + s));
-
-    String[] key;
-    String line;
-    while (Objects.nonNull((line = subFileBufferedReader.readLine()))) {
-      String[] lineArray = line.split(",", -1);
-      Product product = new Product();
-      product.setType(dataLoadService.getProductType(lineArray));
-      product.setColor(dataLoadService.getProductPropertiesColor(lineArray));
-      product.setGbLimit(dataLoadService.getProductPropertiesGbLimit(lineArray));
-      product.setPrice(dataLoadService.getPrice(lineArray));
-      product.setAddress(dataLoadService.getStoreAddress(lineArray));
-      product.setCity(dataLoadService.getStoreCity(lineArray));
-
-      System.out.println("product : "+product);
-      Product product1 = dataLoadService.saveProduct(product);
-      productList.add(product1);
-    }
+    List<Product> productList = dataLoadService.loadCSVToDB();
     return new ResponseEntity<>(productList, HttpStatus.OK);
   }
 
--- src/main/java/se/telenor/assignment/api/service/DataLoadService.java
+++ src/main/java/se/telenor/assignment/api/service/DataLoadService.java
@@ -2,14 +2,50 @@ package se.telenor.assignment.api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import se.telenor.assignment.api.Controller.DataLoadRestController;
 import se.telenor.assignment.api.model.Product;
 import se.telenor.assignment.api.repository.ProductRepository;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 @Component
 public class DataLoadService {
 
     @Autowired
     private ProductRepository productRepository;
+
+    public List<Product> loadCSVToDB() throws Exception{
+        List<Product> productList = new ArrayList<>();
+        BufferedReader subFileBufferedReader =
+                new BufferedReader(
+                        new InputStreamReader(DataLoadRestController.class.getResourceAsStream("/data.csv")));
+        String[] subFileColumnNames = subFileBufferedReader.readLine().split(",");
+        System.out.println("subFileColumnNames: " + subFileColumnNames.length);
+        Arrays.stream(subFileColumnNames).forEach(s -> System.out.println("Value :: " + s));
+
+        String[] key;
+        String line;
+        while (Objects.nonNull((line = subFileBufferedReader.readLine()))) {
+            String[] lineArray = line.split(",", -1);
+            Product product = new Product();
+            product.setType(getProductType(lineArray));
+            product.setColor(getProductPropertiesColor(lineArray));
+            product.setGbLimit(getProductPropertiesGbLimit(lineArray));
+            product.setPrice(getPrice(lineArray));
+            product.setAddress(getStoreAddress(lineArray));
+            product.setCity(getStoreCity(lineArray));
+
+            System.out.println("product : "+product);
+            Product product1 = saveProduct(product);
+            productList.add(product1);
+        }
+        return productList;
+    }
 
     public String getProductType(String[] lineArray) {
         String value = lineArray[0];
